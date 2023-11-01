58ae89b04692b81eb96e5093c2df21f3a8a20010
Natã Belo <natabsv.carioca@gmail.com>
2023-11-01 20:43:46 -0300

    Update rowdiceT.c
    Now it's possible to choose how many times each thread will roll the dice, and the program return the frequency that each die face showed up for each thread.

--- Multithreading programs/rowdiceT.c
+++ Multithreading programs/rowdiceT.c
@@ -1,15 +1,18 @@
 /*****************************************************************************************
-*    A program that creates "n" threads(based on how many of them you want)              *
-*    with the <pthread> API of UNIX systems, witch each of them rows a dice once         *
-*    and generates a different value each time the program runs.                         *                                                                   *
+*    A program that creates "n" threads(based on how many of them you want and at your   *
+*    own risk) with the <pthread> API of UNIX systems, witch each of them rows a dice    *
+*    how many times you want and generates the frequency that each die was rolled up     *
+*    by each thread.                                                                     *
 *****************************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <pthread.h>
+#include <string.h>
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+int numRolls = 0;
 
 void print_dice(int value)
 {
@@ -75,15 +78,61 @@ void print_dice(int value)
 
 void* row_dice(void* arg)
 {
-    int num;
-    
+    int num, sum;
+    int freq[6] = {0};
+    int count = 0;
+    char st[3] = "st";
+    char nd[3] = "nd";
+    char rd[3] = "rd";
+    char th[3] = "th";
+
     pthread_mutex_lock(&lock);
 
-    printf("%s:\n", (char*) arg);
+    for(int i=0; i<strlen((char*)arg)+7; i++)
+    {
+        printf("-");
+    }
 
-    num = (rand() % 6) + 1;
+    printf("\n");
 
-    print_dice(num);
+    for(int i=0, b=strlen((char*)arg)+7; ; i++)
+    {
+        printf(" ");
+        if(i == (b/4)-1)
+        {
+            printf("%s\n", (char*)arg);
+            break;
+        }
+    } 
+    //printf("%s\n", (char*)arg);
+
+    for(int i=0; i<strlen((char*)arg)+7; i++)
+    {
+        printf("-");
+    }
+    printf("\n");
+
+    do
+    {
+        num = (rand() % 6) + 1;
+        freq[num-1] += 1;
+        sum += num;
+
+        printf("%5d%s Roll: %d\n", count+1, ((count+1) == 1) ? st : ((count+1) == 2) ? nd : ((count+1) == 3) ? rd : th, num);
+        
+        count++;
+
+    }while(count < numRolls);
+
+    printf("\nFrequency:\n\n");
+
+    for(int i=0; i<6; i++)
+    {
+        printf("(%d) ", i+1);
+        printf("%.2f%%\n", ((float)freq[i] / numRolls) * 100.0);
+        print_dice(i+1);
+        
+    }
 
     pthread_mutex_unlock(&lock);
     
@@ -98,13 +147,13 @@ int main()
 
     srand(time(NULL));
 
-    printf("-----main Begin-----\n");
+    printf("-----Main Begin-----\n");
 
     int isNum = 0;
     do
     {
         isNum = 1;
-        printf("Number of threads that will row the dice once: ");
+        printf("Number of threads: ");
         fgets(buffer, 80, stdin);
 
         for(int i=0; buffer[i]; i++)
@@ -124,8 +173,36 @@ int main()
             
         }
     }while(!isNum);
-
+    
     numThreads = atoi(buffer);
+
+    isNum = 0;
+    do
+    {
+        isNum = 1;
+        printf("Number of Rollings for each Thread: ");
+        fgets(buffer, 80, stdin);
+
+        for(int i=0; buffer[i]; i++)
+        {
+            if(buffer[0] == '\n' || buffer[0] == '0')
+            {
+                printf("The dice must be rolled at least 1 time!\n");
+                isNum = 0;
+                break;
+            }
+            else if(!(buffer[i] >= '0' && buffer[i] <= '9') && !(buffer[i] == '\n'))
+            {
+                printf("Only numbers are acepted in this field!\n");
+                isNum = 0;
+                break;
+            }
+            
+        }
+    }while(!isNum);
+
+    numRolls = atoi(buffer);
+
     th = malloc(numThreads*sizeof(pthread_t));
     char threadArgs[numThreads][10];
     
